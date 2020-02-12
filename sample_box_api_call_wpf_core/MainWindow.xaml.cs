72e271c81e461aac6feadd233c603bacfabf41db
suusanex <suusan@himado.net>
2020-02-12 19:31:34 +0900

    途中

--- sample_box_api_call_wpf_core/MainWindow.xaml.cs
+++ sample_box_api_call_wpf_core/MainWindow.xaml.cs
@@ -58,6 +58,7 @@ namespace sample_box_api_call_wpf_core
         }
 
         internal BindingSource m_Bind;
+        private Uri m_RedirectUri = new Uri("https://localhost");
 
 
         public MainWindow()
@@ -70,23 +71,66 @@ namespace sample_box_api_call_wpf_core
 
         }
 
-        private void OnBtnGetToken(object sender, RoutedEventArgs e)
-        {
-            var clientId = m_Bind.ClientId;
-            var clientSecret = ClientSecretBox.Password;
-
-            var config = new BoxConfig(clientId, clientSecret, new Uri("http://localhost"));
-            var client = new BoxClient(config);
-            
-
-        }
-
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
             var config = new AppConfig();
             m_Bind.ClientId = config.Config.GetSection("ClientAuthSettings")["ClientId"];
             ClientSecretBox.Password = config.Config.GetSection("ClientAuthSettings")["ClientSecret"];
 
+            Browser.Navigating += BrowserOnNavigating;
         }
+
+
+        private void OnBtnGetToken(object sender, RoutedEventArgs e)
+        {
+            var clientId = m_Bind.ClientId;
+            var clientSecret = ClientSecretBox.Password;
+
+            var config = new BoxConfig(clientId, clientSecret, m_RedirectUri);
+            var client = new BoxClient(config);
+
+            //var authUrl = "https://account.box.com/api/oauth2/authorize";
+
+
+            Browser.Navigate(config.AuthCodeUri);
+
+        }
+
+        private void BrowserOnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.Uri.Host.Equals(m_RedirectUri.Host))
+            {
+
+                //注：コピペ
+                // grab access_token and oauth_verifier
+                IDictionary<string, string> keyDictionary = new Dictionary<string, string>();
+                var qSplit = e.Uri.Query.Split('?');
+                foreach (var kvp in qSplit[qSplit.Length - 1].Split('&'))
+                {
+                    var kvpSplit = kvp.Split('=');
+                    if (kvpSplit.Length == 2)
+                    {
+                        keyDictionary.Add(kvpSplit[0], kvpSplit[1]);
+                    }
+                }
+
+                AuthCode = keyDictionary["code"];
+
+                e.Cancel = true;
+                Dispatcher.Invoke(() => Browser.Visibility = Visibility.Collapsed);
+
+                MessageBox.Show(AuthCode);
+
+
+                //if (AuthCodeReceived != null)
+                //{
+                //    AuthCodeReceived(this, new EventArgs() { });
+                //    oauthBrowser.Visibility = Visibility.Collapsed;
+                //}
+            }
+        }
+
+        public string AuthCode { get; private set; }
+
     }
 }
